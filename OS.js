0cd0c3d695e4fed2655f5cd953e19501a1a408af
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-03-19 22:21:13 +0600

    That's the os.js file

---
+++ OS.js
@@ -0,0 +1,8 @@
+//os module is a node.js module where you can write code about your operating system
+// We need to import the os module to use the os module.
+const os = require("os");
+
+// Cheaking the operating system you are running on your computer :
+console.log(os.platform());
+
+console.log(os.freemem());
